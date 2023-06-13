f11e0aeeb09be3061f38a647970bcad05806a579
Documenter.jl <documenter@juliadocs.github.io>
2023-06-13 18:59:11 +0000

    build based on 5a144a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConstrainedPlanarTriangles","category":"page"},{"location":"#ConstrainedPlanarTriangles","page":"Home","title":"ConstrainedPlanarTriangles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConstrainedPlanarTriangles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConstrainedPlanarTriangles]","category":"page"}]
+}
